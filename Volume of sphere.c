1684a7d2ea116b05380ec56f5d6bbfeb38688a1d
yaejinkong <127467781+yaejinkong@users.noreply.github.com>
2023-03-10 15:36:03 +0900

    Added author and modified the formula

--- Volume of sphere.c
+++ Volume of sphere.c
@@ -1,11 +1,15 @@
 #YaejinKong_201900402_Department of Brazilian Studies
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	float r = 10;
+	float r;
 
-	printf("The volume of a sphere is %f\n", 4.0f / 3.0f * 3.14 * r * r * rr);
+	printf("Enter the num : ");
+	scanf("%f", &r);
+
+	printf("The volume of a sphere with a %f -meter radius is %f", r, 4.0f / 3.0f * 3.14 * r * r * r);
 
 	return 0;
 }
